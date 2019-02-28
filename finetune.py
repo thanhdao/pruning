0e1af05f79bcdb5185d8baa6683991f6eac20c0a
Thanh Dao <thanhdd1@gmail.com>
2019-02-28 18:24:47 +0900

    Update data path for neptune

--- finetune.py
+++ finetune.py
@@ -196,21 +196,21 @@ class PrunningFineTuner_AlexNet:
 
   def accuracy(self,output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
+    # with torch.no_grad():
+    maxk = max(topk)
+    batch_size = target.size(0)
 
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.cpu().eq(target.view(1, -1).expand_as(pred))
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.cpu().eq(target.view(1, -1).expand_as(pred))
 
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.numpy()[0])
+    res = []
+    for k in topk:
+      correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+      res.append(correct_k.numpy()[0])
             # print("***************** Accuracy top K:             ", res)
             # res.append(correct_k.mul_(100.0 / batch_size))
-        return res
+    return res
 
   def test(self):
     print(' PrunningFineTuner_AlexNet test')
